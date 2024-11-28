e7f95d1376ea07ae3618e143410b54c52fe85268
alexdesty <alex.desty@gmail.com>
2024-11-28 13:59:17 +0300

    Add the second task

---
+++ Lesson2Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson2Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            float first_number = Convert.ToSingle(Console.ReadLine());
+            Console.Write("\nEnter the second number: ");
+            float second_number = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("\nResults:");
+            Console.WriteLine($"\n{first_number}+{second_number}={first_number + second_number}");
+            Console.WriteLine($"\n{first_number}-{second_number}={first_number - second_number}");
+            Console.WriteLine($"\n{first_number}*{second_number}={first_number * second_number}");
+            Console.WriteLine($"\n{first_number}/{second_number}={first_number / second_number}");
+
+        }
+    }
+}
