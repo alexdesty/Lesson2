f3fb6f72138a302dea94e3a8b7287694fb183b2e
alexdesty <alex.desty@gmail.com>
2024-11-28 11:47:02 +0300

    Add project

---
+++ Lesson2/Models/AskInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2.Models
+{
+    struct AskInfo
+    {
+        public string Name;
+        public int Age;
+        public float Height;
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lesson2.Models;
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var askInfo1 = new AskInfo();
+            Console.Write("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.Write("\nВведите возраст: ");
+            var age = Console.ReadLine();
+            Console.Write("\nВведите рост: ");
+            var height = Console.ReadLine();
+            askInfo1.Name = name;
+            askInfo1.Age = Convert.ToInt32(age);
+            askInfo1.Height = Convert.ToSingle(height);
+            Console.WriteLine($"\nПривет, {askInfo1.Name}!");
+            Console.WriteLine($"\tВаш возраст: {askInfo1.Age} лет.");
+            Console.WriteLine($"\t\tВаш рост: {askInfo1.Height}м.");
+        }
+    }
+}
